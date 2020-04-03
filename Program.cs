44fe40ac76236e1e9774da2baf9fb918f4af6a80
ElenaRudko <62296055+ElenaRudko@users.noreply.github.com>
2020-04-03 16:56:56 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,93 +3,130 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace laba2
+namespace laba3
 {
-	class Program
-{
-	static void Main(string[] args)
-{    
-	string str;
-	Console.WriteLine("--------------Обратный порядок слов в строке---------------------");
-	Console.WriteLine("\nВведите строку:");
-	string d = Console.ReadLine();
-	string[] words = d.Split(' ');//расщепляет на слова
-	Console.WriteLine("Обратный поряд слов:");
-	Array.Reverse(words);
-	for (int i = 0; i < words.Length; i++)
-{
-	 Console.Write(words[i] + " ");
-}
-	Console.WriteLine("\n--------------Текущие дата и время---------------------");
-	DateTime date = DateTime.Now;
-	string firstDate = date.ToString("F");
-	string secondDate = date.ToString("G");
-	Console.WriteLine("\nПервый формат: " + firstDate);
-	ShowResult(firstDate);
-	Console.WriteLine("\nВторой формат: " + secondDate);
-	ShowResult(secondDate);
-	Console.WriteLine("\n--------------Строка с знаками препинания---------------------");
-	char[]s = new char[] { ',', '.', '-', ':', ';', '!', '?' };
-	Console.Write("Введите строку: ");
-	str = Console.ReadLine();
-	StringBuilder strBuild = new StringBuilder(str);
-	for (int i = 0; i < strBuild.Length; i++)
-    {	
-	bool flag = false;
-	for (int j = 0; j < s.Length; j++)
-     {
-	if (strBuild[i] == punctuationMarks[j])
-     {
-	for (int k = i; k >= 0; k--)
-      {
-	if (strBuild[k] == ' ')
-       {
-	strBuild.Insert(k + 1, s[j]);
-	++i;
-	flag = true;
-	break;	
-	}
-	else if (k == 0)
-       {
-	strBuild.Insert(k, s[j]);
-	++i;
-	flag = true;
-	break;	
-	}
-	}
-	}
-	    if (flag)
-	    break;	
-	}
-	}
-	    str = strBuild.ToString();
-	    Console.Write("Результат: ");
-	    foreach (char c in str)
-	    Console.Write(c);
-	    Console.ReadLine();
-	    Console.ReadKey();	}
-	static void ShowResult(string _date)
-	{ 
-	      int[] result = new int[10];
-	      for (int i = 0; i < _date.Length; i++)
-	{ 
-	      switch (_date[i])
+	class People
 	{
-		case '1': result[1]++; break;
-		case '2': result[2]++; break;
-		case '3': result[3]++; break;
-		case '4': result[4]++; break;
-		case '5': result[5]++; break;
-		case '6': result[6]++; break;
-		case '7': result[7]++; break;
-		case '8': result[8]++; break;
-		case '9': result[9]++; break;
-		case '0': result[0]++; break;
+		protected static int objectsCounter;
+		protected string Name { get; set; }
+		protected string Pol { get; set; }
+		public string this[string st]
+		{
+			get
+			{
+				switch (st)
+				{
+					case "name": return Name;
+					case "pol": return Pol;
+					default: return "не найдено";
+				}
+			}
+			set
+			{
+				switch (st)
+				{
+					case "name":
+						Name = value;
+						break;
+					case "pol":
+						Pol = value;
+						break;
+					default:
+						break;
+				}
+			}
 		}
-	      }
-		Console.WriteLine("\nКоличество ");
-		for (int i = 0; i < result.Length; i++)
-		Console.WriteLine(i + " содержиться " + result[i]+" раз"); 
+		public People(string name, string pol)
+		{
+			Name = name;
+			Pol = pol;
+			objectsCounter++;
+		}
+		public People()
+		{
+			Console.WriteLine("Введите ФИО:");
+			Name = Console.ReadLine();
+			Console.WriteLine("Введите ПОЛ:");
+			Pol = Console.ReadLine();
+			objectsCounter++;
+		}
+		public static void NumberOfObjects()
+		{
+			Console.WriteLine("Number of objects is " + objectsCounter);
+		}
+	}
+	class Student : People
+	{
+		protected int Group { get; set; }
+		public Student(string name, string pol, int group)
+			: base(name, pol)
+		{
+			Group = group;
+		}
+		public Student()
+		{
+			Console.WriteLine("Введите группу:");
+			Group = int.Parse(Console.ReadLine());
+		}
+	}
+	class Speciality : Student
+	{
+		protected string Specialty { get; set; }
+		public Speciality(string name, string pol, int group, string specialty)
+			: base(name, pol, group)
+		{
+			Specialty = specialty;
+		}
+		public Speciality()
+
+		{
+			Console.WriteLine("Введите специальность:");
+			Specialty = Console.ReadLine();
+		}
+		public void OutputInformation()
+		{
+			Console.WriteLine("ФИО: " + Name);
+			Console.WriteLine("Пол: " + Pol);
+			Console.WriteLine("Группа: " + Group);
+			Console.WriteLine("Cпециальность: " + Specialty);
+			Console.WriteLine();
+		}
+		//Перегрузка.
+		public void OutputInformation(params string[] args)
+		{
+			Console.WriteLine("FIO: " + Name);
+			Console.WriteLine("pol: " + Pol);
+			Console.WriteLine("Group: " + Group);
+			Console.WriteLine("Specialitys: " + Specialty);
+			foreach (var x in args)
+			{
+				Console.WriteLine(x);
+			}
+
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			People.NumberOfObjects();
+			Console.WriteLine("Введите количество студентов: ");
+			int kol = int.Parse(Console.ReadLine());
+			Speciality[] mas = new Speciality[kol];
+			for (int i = 0; i < kol; i++)
+			{
+				Console.WriteLine("Введите информацию о {0} студенте ", i + 1);
+				mas[i] = new Speciality();
+				Console.WriteLine("*********************************");
+			}
+			for (int i = 0; i < kol; i++)
+			{
+				mas[i].OutputInformation("Прошла перегрузка\n");
+			}
+			People.NumberOfObjects();
+			Console.WriteLine(mas[0]["name"]);
+			mas[0]["name"] = "Лена";
+			Console.ReadLine();
 		}
 	}
 }
