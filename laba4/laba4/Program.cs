b202df5ae259f8e36a0049d2c263b1f816903b79
ElenaRudko <62296055+ElenaRudko@users.noreply.github.com>
2020-04-29 01:43:02 +0300

    Add files via upload

---
+++ laba4/laba4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+namespace ConsoleApp15
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Assembly dll = Assembly.Load("dllcsharp");
+			Type tp = dll.GetType("dllcsharp.BasicInfo");
+			object ob = Activator.CreateInstance(tp);
+			MethodInfo mi;
+			mi = tp.GetMethod("print");
+			mi.Invoke(ob, null);
+			Console.ReadKey();
+		}
+	}
+}
