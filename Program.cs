fb4e83467df88b9011231105133b265bd5207314
ElenaRudko <62296055+ElenaRudko@users.noreply.github.com>
2020-04-29 14:49:58 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,34 +1,127 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.Management;
-namespace LR4
+namespace ConsoleApp1
 {
-	class Program
+	enum Education
 	{
-		static void Main(string[] args)
+		Full_Time,
+		Distance,
+		Evening,
+		Magistracy
+	}
+	class Person
+	{
+		public static int Counter
 		{
-			foreach (var drive in DriveInfo.GetDrives())
+			get
 			{
-				try
-				{
-					Console.WriteLine("Имя диска: " + drive.Name);
-					Console.WriteLine("Файловая система: " + drive.DriveFormat);
-					Console.WriteLine("Тип диска: " + drive.DriveType);
-					Console.WriteLine("Объем доступного свободного места (в байтах): " + drive.AvailableFreeSpace);
-					Console.WriteLine("Готов ли диск: " + drive.IsReady);
-					Console.WriteLine("Корневой каталог диска: " + drive.RootDirectory);
-					Console.WriteLine("Общий объем свободного места, доступного на диске (в байтах): " + drive.TotalFreeSpace);
-					Console.WriteLine("Размер диска (в байтах): " + drive.TotalSize);
-					Console.WriteLine("Метка тома диска: " + drive.VolumeLabel);
-					Console.WriteLine();
-				}
-				catch { }
+				return objectsCounter;
 			}
-			Console.ReadLine();
+		}
+		private static int objectsCounter;
+		protected struct Student
+        {
+            string fio;
+		public string FIO
+		{
+			get { return fio; }
+			set
+			{
+			    fio = value;				
+			}
 		}
 	}
+	public string Education;
+	protected Student student;
+	public Person(string fio,  int education)
+	{
+		student.FIO = fio;
+		Education = Enum.GetName(typeof(Education), education);
+		objectsCounter++;
+		}
+	public void Information()
+	{
+		Console.WriteLine("ФИО студента: " + student.FIO);
+		Console.WriteLine("Вид обучения: " + Education);
+		objectsCounter++;
+		}
+		public virtual void Uni() { }
+		public static void Number()
+		{
+			Console.WriteLine("Количество студентов: " + objectsCounter);
+		}
+	}
+class Programmer : Person
+	{
+		int Course;
+	public Programmer( string fio,  int education, int course) : base( fio, education)
+	{
+		Course = course;
+	}
+	public new void Information()
+	{
+		Console.WriteLine("ФИО студента: " + student.FIO);
+		Console.WriteLine("Вид обучения: " + Education);
+		Console.WriteLine("Курс: " + Course);
+		Console.WriteLine("---------------------------------------------------- " );
+		}
+		public override void Uni()
+		{
+			Console.WriteLine("Университет: БГУИР");
+		}
+	}
+class Engineer : Person
+	{
+		string Faculty;
+	public Engineer(string fio,  int education, string faculty) : base( fio, education)
+	{
+		Faculty = faculty;
+	}
+	public new void Information()
+	{
+		Console.WriteLine("ФИО студента: " + student.FIO);
+		Console.WriteLine("Вид обучения: " + Education);
+		Console.WriteLine("Факультет: " + Faculty);
+		Console.WriteLine("---------------------------------------------------- ");
+		}
+		public override void Uni()
+		{
+			Console.WriteLine("Университет: БГУИР");
+		}
+	}
+class Tester : Person
+	{
+		int Age;
+	public Tester( string fio,  int education, int age) : base( fio, education)
+	{
+		Age = age;
+	}
+	public new void Information()
+	{
+		Console.WriteLine("ФИО студента: " + student.FIO);
+		Console.WriteLine("Вид обучения: " + Education);
+		Console.WriteLine("Возраст: " + Age);
+		Console.WriteLine("---------------------------------------------------- ");
+		}
+		public override void Uni()
+		{
+			Console.WriteLine("Университет: БГУИР");
+		}
+	}
+class Program
+{
+	static void Main(string[] args)
+	{
+		Programmer programmer = new Programmer( "Рудько Елена Сергеевна", 2,4);
+		Engineer engineer = new Engineer( "Пульмановская Екатерина Дмитриевна",  1, "ФРЭ");
+		Tester tester = new Tester( "Петроченко Маргарита Сергеевна",  3, 20);
+		programmer.Uni();
+		programmer.Information();
+		engineer.Uni();
+		engineer.Information();
+		tester.Uni();
+		tester.Information();
+		Person.Number();
+		Console.ReadLine();
+	}
+  }
 }
